13c98c5b4bea5f62e875ee2e4bed3cc00de079ac
jj-hyunee <jj.hyun52.6@gmail.com>
2022-10-02 17:10:17 +0900

    bottom navigation bar

---
+++ app/src/main/java/com/example/graduationproject/Select2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.graduationproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Select2Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select2)
+    }
+}
