286a13572213da7caace35acbcf549f5c27059f6
gijin <gijin100@naver.com>
2022-11-07 00:13:55 +0900

    add Style Model

---
+++ app/src/main/java/com/example/graduationproject/AddDataActivity.kt
@@ -0,0 +1,161 @@
+package com.example.graduationproject
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.graduationproject.classfier.YoloClassfier
+import com.example.graduationproject.classfier.YoloInterfaceClassfier
+import com.example.graduationproject.databinding.ActivityAddDataBinding
+
+import com.example.graduationproject.env.ImageUtils
+import com.example.graduationproject.tracker.MultiBoxTracker
+import java.io.File
+import java.io.IOException
+
+class AddDataActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityAddDataBinding
+    private lateinit var bitmap: Bitmap
+
+
+    lateinit var resultList : List<YoloInterfaceClassfier.Recognition>
+    //의상 검출을 위한 변수
+    lateinit var detector: YoloClassfier
+
+    //특정 구역이 40%이상의 확률로 특정 카테고리로 판정할 때 쓰는 변수
+    val MINIMUM_CONFIDENCE_TF_OD_API=0.4f
+
+    //가로 세로 길이용 변수
+    protected var previewWidth = 0
+    protected var previewHeight = 0
+    val TF_OD_API_INPUT_SIZE = 416
+
+    private var frameToCropTransform: Matrix? = null
+    private var cropToFrameTransform: Matrix? = null
+    lateinit var tracker: MultiBoxTracker
+
+    //이미지 잘라내기를 위한 변수인데,
+    //이미지를 바로 잘라내지 않고 나중에 잘라내기 위해 false로 했습니다.
+    private val MAINTAIN_ASPECT = false
+
+    //회전과 관련된 변수
+    private val sensorOrientation = 90
+
+    //양자화
+    //모델을 가볍게 쓰기 위해 양자화를 쓰는 경우도 있긴한데, 전 안씁니다.
+    private val TF_OD_API_IS_QUANTIZED = false
+
+    //나중에 모델 더 좋게 학습하면 모델이름을 바꾸거나 업데이트하겠죠.
+    //모델 이름과 경로, obj.txt는 이미지의 카테고리입니다(셔츠, 팬츠 같은)
+    private val TF_OD_API_MODEL_FILE = "yolov4_2.tflite"
+
+    private val TF_OD_API_LABELS_FILE = "file:///android_asset/obj.txt"
+    //검출하는 부분은 여기다가 두었습니다.
+    var mappping = mutableMapOf<Int, String>(0 to "탑", 1 to "블라우스", 2 to "티셔츠", 3 to "니트웨어",
+        4 to "셔츠", 5 to "브라탑", 6 to "후드티", 7 to "청바지", 8 to "팬츠", 9 to "스커트",
+        10 to "레깅스", 11 to "조거팬츠" , 12 to "코트",  13 to "재킷", 14 to "점퍼", 15 to "패딩",
+        16 to "베스트", 17 to "가디건", 18 to "짚업", 19 to "드레스" , 20 to "점프수트"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityAddDataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val cacheFile = File(cacheDir, "cropped.jpg").path
+        bitmap=BitmapFactory.decodeFile(cacheFile)
+
+        initbox()
+        binding.imageView2.setImageBitmap(bitmap)
+        init()
+    }
+
+
+    fun init(){
+        val style_items = resources.getStringArray(R.array.style_array)
+        val category_items=resources.getStringArray(R.array.category_array)
+
+        val myadapter_style=ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,style_items)
+        binding.styleSpinner.adapter=myadapter_style
+
+        val myadapter_category=ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,category_items)
+        binding.categorySpinner.adapter=myadapter_category
+
+    }
+
+    private fun initbox() {
+        previewHeight = 416
+        previewWidth = 416
+
+        //matrix 416x416 행렬이 생겼다 정도로 이해하면 될듯합니다.
+        frameToCropTransform = ImageUtils.getTransformationMatrix(
+            previewWidth, previewHeight,
+            TF_OD_API_INPUT_SIZE, TF_OD_API_INPUT_SIZE,
+            sensorOrientation, MAINTAIN_ASPECT
+        )
+
+        cropToFrameTransform = Matrix()
+        //역행렬 찾음 cropToFrameTramsForm에.
+        frameToCropTransform!!.invert(cropToFrameTransform)
+
+
+        //activity Detect에, trackingOverlay가 있는데,
+        //handleResult에 canvas.drawRect(location, paint)의 주석을 지우면 아래가 작동하여, 네모를 그립니다.
+        tracker = MultiBoxTracker(this)
+
+        //기본 설정입니다.
+        tracker.setFrameConfiguration(
+            TF_OD_API_INPUT_SIZE,
+            TF_OD_API_INPUT_SIZE,
+            sensorOrientation
+        )
+        try {
+            Log.i("main fail :","check")
+            //YoloDetector를 만드는 부분입니다. 416x416 사이즈에, 양자화를 사용하지 않는다- 라는 걸 넘겨줍니다.
+            //asset은 학습한 모델의 경로를 알기 위해 넘겨줍니다.
+            detector = YoloClassfier().create(
+                assets,
+                TF_OD_API_MODEL_FILE,
+                TF_OD_API_LABELS_FILE,
+                TF_OD_API_IS_QUANTIZED
+            )
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.i("main fail :","Exception initializing classifier!")
+            Toast.makeText(
+                applicationContext, "Classifier could not be initialized", Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+        Toast.makeText(this,"의상 검출",Toast.LENGTH_SHORT).show()
+        //비동기적으로
+        //의상 탐지를 위해 detector에 bitmap이미지를 넣습니다.
+        val results: List<YoloInterfaceClassfier.Recognition>? = detector.recognizeImage(bitmap)
+        if (results!=null){
+            resultList=results
+        }else{
+            Toast.makeText(this,"의상검출 실패",Toast.LENGTH_SHORT).show()
+        }
+        var max_idx=0
+        var max_confidence:Float=0.0F
+        for ((idx,result) in resultList.withIndex()){
+            if(result.confidence!! >max_confidence){
+                max_idx=idx
+                max_confidence=result.confidence
+            }
+        }
+        Toast.makeText(this,mappping[resultList[max_idx].detectedClass],Toast.LENGTH_SHORT).show()
+        binding.editTextT1.setText(mappping[resultList[max_idx].detectedClass])
+
+
+
+    }
+}
--- app/src/main/java/com/example/graduationproject/AddFragment.kt
+++ app/src/main/java/com/example/graduationproject/AddFragment.kt
@@ -1,5 +1,6 @@
 package com.example.graduationproject
 
+import android.app.Activity
 import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -7,64 +8,55 @@ import androidx.fragment.app.Fragment
 import com.example.graduationproject.databinding.FragmentAddBinding
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
-
-
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
+
+
 class AddFragment: Fragment(R.layout.fragment_add) {
     private var _binding: FragmentAddBinding? =null
     private val binding get()=_binding!!
     lateinit var bitmap: Bitmap
+    lateinit var bitmap2:Bitmap
 
     //사진 사이즈를 416으로 정하겠다는 의미입니다.
     val TF_OD_API_INPUT_SIZE = 416
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = com.example.graduationproject.databinding.FragmentAddBinding.inflate(inflater,container,false)
         val activity = activity as MainActivity
 
 
         val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//            val contentResolver1=activity.contentResolver;
-//
-//            bitmap= Utils.processBitmap(
-//                    ImageDecoder.decodeBitmap(
-//                        ImageDecoder.createSource(contentResolver1,uri!!),
-//                        ImageDecoder.OnHeaderDecodedListener { decoder, info, source ->
-//                            decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
-//                            decoder.isMutableRequired = true
-//                        }),TF_OD_API_INPUT_SIZE)
+
             val uri :Intent=it.data as Intent
             cropImage(uri.data)
             Log.d("mmm ddk", CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE.toString())
 
         }
-        val getCameras = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if(it.resultCode==RESULT_OK && it.data != null){
-                val extras=it.data!!.extras
-                bitmap=extras?.get("data") as Bitmap
 
-            }
-        }
         binding.cancelButton.setOnClickListener {
             binding.imageView.visibility=View.INVISIBLE
             binding.buttonLinearLayout.visibility=View.INVISIBLE
-
             binding.addCameraLayout.visibility=View.VISIBLE
             binding.addGalleryLayout.visibility=View.VISIBLE
 
@@ -73,16 +65,17 @@ class AddFragment: Fragment(R.layout.fragment_add) {
             val storage: File = activity.cacheDir
             val fileName: String = "cropped.jpg"
             val tempFile = File(storage, fileName)
-
+//            val fileName2: String = "uncropped.jpg"
+//            val tempFile2=File(storage,fileName2)
             try {
                 // 자동으로 빈 파일을 생성합니다.
                 tempFile.createNewFile()
+//                tempFile2.createNewFile()
                 // 파일을 쓸 수 있는 스트림을 준비합니다.
                 val out = FileOutputStream(tempFile)
-
+//                val out2=FileOutputStream(tempFile2)
                 // compress 함수를 사용해 스트림에 비트맵을 저장합니다.
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-
                 // 스트림 사용후 닫아줍니다.
                 out.close()
             } catch (e: FileNotFoundException) {
@@ -100,7 +93,7 @@ class AddFragment: Fragment(R.layout.fragment_add) {
         binding.addCameraLayout.setOnClickListener {
             Log.i("qwer","CameraLayout")
             val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            getCameras.launch(intent)
+            getContent.launch(intent)
         }
         binding.addGalleryLayout.setOnClickListener {
             Log.i("qwer2","GalleryLayout")
@@ -114,4 +107,43 @@ class AddFragment: Fragment(R.layout.fragment_add) {
 
         return binding.root
     }
+
+    private fun cropImage(uri: Uri?) {
+        val crop =CropImage.activity(uri).setGuidelines(CropImageView.Guidelines.ON)  // 크롭 위한 가이드 열어서 크롭할 이미지 받아오기
+            .setCropShape(CropImageView.CropShape.RECTANGLE)            // 사각형으로 자르기
+            .start(activity as MainActivity,this@AddFragment)
+
+        // 프레그먼트에서 사용할 땐 .start(activity as 프레그먼트의 부모 Activity, this@형재 프레그먼트 이름)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            // 크롭해서 프로필 사진 설정하기
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    result.uri?.let {
+                        // 이미지 파일 읽어와서 설정하기
+
+                        bitmap = BitmapFactory.decodeStream(activity?.contentResolver!!.openInputStream(result.uri!!))
+                        bitmap=Bitmap.createScaledBitmap(bitmap,416,416,true)
+                        binding.imageView.setImageBitmap(bitmap)
+                        binding.addCameraLayout.visibility=View.GONE
+                        binding.addGalleryLayout.visibility=View.GONE
+                        binding.buttonLinearLayout.visibility=View.VISIBLE
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        _binding=null;
+        super.onDestroyView()
+
+    }
+
 }
